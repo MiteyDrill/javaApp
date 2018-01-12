190c0a9d26ade4390303a77b06d0bfaa54d57fa6
Benjamin Sanchez <Linkandcheezer@gmail.com>
2018-01-12 11:44:06 -0800

    -Increment Buttons and Logic
    -Added some '+' and '-' buttons to increment the amount of coffee
    wanted.
    -Added some more logic to the calculate button.

--- justJava/app/src/main/java/com/benjaminsanchezthethird/justjava/MainActivity.java
+++ justJava/app/src/main/java/com/benjaminsanchezthethird/justjava/MainActivity.java
@@ -12,6 +12,10 @@ import android.widget.TextView;
  */
 public class MainActivity extends AppCompatActivity {
 
+    //global variable
+    int numOfCoffee = 0;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,8 +25,29 @@ public class MainActivity extends AppCompatActivity {
     /**
      * This method is called when the order button is clicked.
      */
+
+    public void increaseCoffee (View view){
+
+        numOfCoffee++;
+
+        display(numOfCoffee);
+    }
+
+    public void decreaseCoffee (View view){
+
+        numOfCoffee--;
+
+        display(numOfCoffee);
+    }
+
+
     public void submitOrder(View view) {
-        display(1);
+
+        //variable holds the amount of coffee ordered
+
+        //sends number to function that'll display price on screen
+        display(numOfCoffee);
+        displayPrice(numOfCoffee * 2);
     }
 
     /**
@@ -31,5 +56,16 @@ public class MainActivity extends AppCompatActivity {
     private void display(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.total_number);
         quantityTextView.setText("" + number);
-    }
+
+     }
+
+     private void displayPrice(int num){
+
+        //sets variable
+        TextView priceTextView = (TextView) findViewById(R.id.price_number);
+
+        //sets variable to that price.
+        priceTextView.setText("$ " + num);
+
+     }
 }
